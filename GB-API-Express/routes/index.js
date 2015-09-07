5ca1bc5cf3fe7433af16c52a6d16212362dd9062
arditesbd <zularbine@arditesbd.com>
2015-09-07 19:55:25 +0600

    updated the code

--- GB-API-Express/routes/index.js
+++ GB-API-Express/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    //res.render('index', { title: 'Express' });
     res.json({message: 'hooray! welcome to our app'});
 });
 
