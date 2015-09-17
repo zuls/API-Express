0224e43e7a92c3eea8e0b70d9d93f74fd35a6856
arditesbd <zularbine@arditesbd.com>
2015-09-17 19:20:25 +0600

    Test

--- GB-API-Express/routes/index.js
+++ GB-API-Express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.json({message: 'hooray! welcome to our app'});
+    res.json({message: 'hooray! welcome to our app This is auosome !'});
 });
 
 module.exports = router;
